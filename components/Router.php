0f2d2ef7b24cf7a8ff4f522cc05d7cd42b1c7b0f
David <rotokan2@gmail.com>
2017-12-14 16:47:22 +0300

    Init_setting

---
+++ components/Router.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: David Arakelyan
+ * Email: rotokan2@gmail.com
+ * Date: 14.12.2017
+ * Time: 16:35
+ */
+
+class Router
+{
+
+}
---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: David Arakelyan
+ * Email: rotokan2@gmail.com
+ * Date: 14.12.2017
+ * Time: 15:55
+ */
+
+//Front Controller
+
+//1. Общие настройки
+ini_set('display_errors',1);
+error_reporting(E_ALL);
+
+//2.Подключение файлов системы
+define('ROOT', dirname( __FILE__));
+require_once (ROOT . '/components/Router.php');
+
+
+//3. Подключение к базе данных
+
+//4. Вызов Router
